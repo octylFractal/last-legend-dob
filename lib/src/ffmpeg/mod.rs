c5628e7da4dbb656623675699f789d5d9ad84c64
Octavia Togami <octavia.togami@gmail.com>
2022-01-30 10:13:09 -0800

    Some small improvements to output FLACs

--- lib/src/ffmpeg/mod.rs
+++ lib/src/ffmpeg/mod.rs
@@ -1,3 +1,4 @@
+use std::fs::File;
 use std::io::{ErrorKind, Read, Write};
 use std::ops::{Deref, DerefMut};
 use std::process::{Child, Command, Output, Stdio};
@@ -81,24 +82,42 @@ pub fn loop_using_metadata(
         (loop_start, loop_end)
     };
 
-    // Run FFMPEG command to loop the audio
-    let ffmpeg_args = parse_command_args(&*format!(
-        "{general} {loglevel} -y -i {file} -af aloop=loop=1:start={loop_start}:size={loop_size} -f flac {file2}",
-        general = GENERAL_FFMPEG_INSTRUCTIONS,
-        loglevel = get_ffmpeg_loglevel(),
-        file = original_cache_file.path().display(),
-        loop_start = loop_start,
-        loop_size = loop_end - loop_start,
-        file2 = looped_cache_file.path().display(),
-    ));
-    log::debug!("Running ffmpeg {:?}", ffmpeg_args);
-    let ffmpeg_loop_output = Command::new("ffmpeg")
-        .args(ffmpeg_args)
-        .stdin(Stdio::null())
-        .stdout(Stdio::null())
-        .output()
-        .map_err(|e| LastLegendError::Io("Couldn't run ffmpeg".into(), e))?;
-    check_exit(&ffmpeg_loop_output)?;
+    // Run FFMPEG command to loop the audio (if the loop point isn't just 0)
+    match loop_start {
+        0 => {
+            // We can just do an in-process file copy
+            std::io::copy(
+                &mut File::open(original_cache_file.path()).map_err(|e| {
+                    LastLegendError::Io("Couldn't open original cache file".into(), e)
+                })?,
+                &mut File::create(looped_cache_file.path()).map_err(|e| {
+                    LastLegendError::Io("Couldn't open looped cache file".into(), e)
+                })?,
+            )
+            .map_err(|e| {
+                LastLegendError::Io("Couldn't copy original file to looped file".into(), e)
+            })?;
+        }
+        _ => {
+            let ffmpeg_args = parse_command_args(&*format!(
+                "{general} {loglevel} -y -i {file} -af aloop=loop=1:start={loop_start}:size={loop_size} -f flac {out_file}",
+                general = GENERAL_FFMPEG_INSTRUCTIONS,
+                loglevel = get_ffmpeg_loglevel(),
+                file = original_cache_file.path().display(),
+                loop_start = loop_start,
+                loop_size = loop_end - loop_start,
+                out_file = looped_cache_file.path().display(),
+            ));
+            log::debug!("Running ffmpeg {:?}", ffmpeg_args);
+            let ffmpeg_loop_output = Command::new("ffmpeg")
+                .args(ffmpeg_args)
+                .stdin(Stdio::null())
+                .stdout(Stdio::null())
+                .output()
+                .map_err(|e| LastLegendError::Io("Couldn't run ffmpeg".into(), e))?;
+            check_exit(&ffmpeg_loop_output)?;
+        }
+    }
 
     // Run FFMPEG command to tell me what the length is
     let probe_args = parse_command_args(&*format!(
@@ -116,7 +135,9 @@ pub fn loop_using_metadata(
         .map_err(|e| LastLegendError::Io("Couldn't run ffprobe".into(), e))?;
     check_exit(&audio_probe_output)?;
     let audio_len: f64 = {
-        let duration = String::from_utf8_lossy(&audio_probe_output.stdout).trim().to_string();
+        let duration = String::from_utf8_lossy(&audio_probe_output.stdout)
+            .trim()
+            .to_string();
         duration.parse().map_err(|_| {
             LastLegendError::FFMPEG(format!("audio duration wasn't a float but: {}", duration))
         })?
@@ -124,36 +145,28 @@ pub fn loop_using_metadata(
 
     // Run FFMPEG command to taper the end since most rolls are intended to "loop forever".
     let ffmpeg_args = parse_command_args(&*format!(
-        "{general} {loglevel} -i {file} -af afade=t=out:st={start}:d=5 -f flac pipe:",
+        "{general} {loglevel} -y -i {file} -af afade=t=out:st={start}:d=5 -f flac {out_file}",
         general = GENERAL_FFMPEG_INSTRUCTIONS,
         loglevel = get_ffmpeg_loglevel(),
         file = looped_cache_file.path().display(),
         start = audio_len - 5f64,
+        out_file = original_cache_file.path().display(),
     ));
     log::debug!("Running ffmpeg {:?}", ffmpeg_args);
-    let mut child = ChildDropGuard(
-        Command::new("ffmpeg")
-            .args(ffmpeg_args)
-            .stdin(Stdio::null())
-            .stdout(Stdio::piped())
-            .spawn()
-            .map_err(|e| LastLegendError::Io("Couldn't spawn ffmpeg".into(), e))?,
-    );
+    let ffmpeg_taper_output = Command::new("ffmpeg")
+        .args(ffmpeg_args)
+        .stdin(Stdio::null())
+        .stdout(Stdio::null())
+        .output()
+        .map_err(|e| LastLegendError::Io("Couldn't run ffmpeg".into(), e))?;
+    check_exit(&ffmpeg_taper_output)?;
 
-    let mut child_stdout = child.0.stdout.take().unwrap();
-    let pull_r = std::io::copy(&mut child_stdout, &mut output);
-    let exit = child
-        .0
-        .wait()
-        .map_err(|e| LastLegendError::Io("Couldn't wait for ffmpeg".into(), e))?;
-    check_exit(&Output {
-        status: exit,
-        stderr: Vec::new(),
-        stdout: Vec::new(),
-    })?;
-    // Throw copy errors after child error,
-    // since if the child crashes its error is more meaningful.
-    pull_r.map_err(|e| LastLegendError::Io("Couldn't pull data from ffmpeg".into(), e))?;
+    std::io::copy(
+        &mut File::open(original_cache_file.path())
+            .map_err(|e| LastLegendError::Io("Couldn't open original cache file".into(), e))?,
+        &mut output,
+    )
+    .map_err(|e| LastLegendError::Io("Couldn't copy from original cache file".into(), e))?;
 
     Ok(())
 }
